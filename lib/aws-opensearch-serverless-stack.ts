3ad09c802bea32e4d77e77db4a516605c229485a
Trung Dinh <xamarindevelopervietnam@outlook.com>
2024-12-14 17:45:59 +0700

    Create interface AwsOpensearchServerlessStackProps

--- lib/aws-opensearch-serverless-stack.ts
+++ lib/aws-opensearch-serverless-stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { AwsOpensearchServerlessStackProps } from './AwsOpensearchServerlessStackProps';
 
 export class AwsOpensearchServerlessStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: AwsOpensearchServerlessStackProps) {
     super(scope, id, props);
   }
 }
